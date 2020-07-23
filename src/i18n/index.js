2ab5a127b012507e15c0db06dbafd5e5f6f72c4d
nchuv <natalya.chuvi@gmail.com>
2020-07-23 21:58:33 +0300

    start;

---
+++ src/i18n/index.js
@@ -0,0 +1,202 @@
+module.exports = {
+  tshirts: `T-shirts`,
+  write: `Write us`,
+  menu: [
+    {
+      title: `About`,
+      link: "link",
+    },
+    {
+      title: `Conferences`,
+      link: "link",
+    },
+    {
+      title: `Services`,
+      link: "link",
+    },
+    {
+      title: `Podcasts`,
+      link: "link",
+    },
+    {
+      title: `YouTube`,
+      link: "link",
+    },
+    {
+      title: `Our team`,
+      link: "link",
+    },
+    {
+      title: `Feedback`,
+      link: "link",
+    },
+    {
+      title: `Partnership`,
+      link: "link",
+    },
+    {
+      title: `Contacts`,
+      link: "link",
+    },
+  ],
+  hero: {
+    title: `Your event creation is within the grasp.`,
+    text: `Konfy - comfortable way to run your conference`,
+    button: `Start`,
+  },
+  about: {
+    text: `Konfy is organizing online conferences and bringing communities together!`,
+  },
+  conferences: {
+    title: `Our conferences`,
+    ticket: `Get free ticket`,
+    list: [
+      {
+        text: `<p>Haskell Love is an online conference for Haskell developers.<br/>The conference talks are devoted to:</p>
+          <ul>
+            <li>FP ecosystem with special emphasis on Haskell language;</li>
+            <li>Opensource;</li>
+            <li>Category Theory;</li>
+            <li>Science of Computer Programming;</li>
+          </ul>
+        </p> `,
+        twitter: `twitter`,
+        patreon: `patreon`,
+        site: `site`,
+        link: `link`,
+      },
+      {
+        text: `<p>Scala Love is an online event gathering FP and OOP programmers all around the world. The event took place on April 18th, 2020.<br/>The conference's lineup was packed with talks about:</p>
+          <ul>
+            <li>Scala, the language which supports both, object-oriented programming and functional programming;</li>
+            <li>Language tooling;</li>
+            <li>The implementation's tips & tricks;</li>
+            <li>FP community building.</li>
+          </ul>
+        </p> `,
+        twitter: `twitter`,
+        patreon: `patreon`,
+        site: `site`,
+      },
+    ],
+  },
+  services: {
+    title: `Services`,
+    cards: [
+      {
+        title: "Full Package",
+        img: "full",
+        list: [
+          "Registration & newsletter services;",
+          "Marketing and social media automatization solution;",
+          "Streaming & recording software;",
+        ],
+      },
+      {
+        title: "Medium Package",
+        img: "medium",
+        list: [
+          "Registration & newsletter tool;",
+          "Marketing and social media promotion",
+        ],
+      },
+      {
+        title: "Light Package",
+        img: "light",
+        list: ["Registration & newsletter tool;"],
+      },
+    ],
+  },
+  podcasts: {
+    title: `Our podcasts`,
+    listen: `Listen`,
+    list: [
+      {
+        text: `<p>A podcast about the Scala Programming Language and its community</p>
+          <p>Scala online broadcast dedicated to developers and fans of FP;
+          <ul>
+            <li>Podcast's guests talk about their day-to-day work;</li>
+            <li>The community and the language in general;</li>
+            <li>every episode brings a huge dose of programming news;</li>
+          </ul>
+        </p> `,
+        twitter: "twitter",
+        link: "link",
+      },
+      {
+        text: `
+          <p>Individuals from all around the world share their passion and commitment given to software craftsmanship.
+            <ul>
+              <li>Various topics covered by experts;</li>
+              <li>Solutions and tools, discussed by its creators and maintainers;</li>
+              <li>Chats about situational and environmental (a history & a community) context of software development.</li>
+            </ul>
+          </p> `,
+        twitter: "twitter",
+        link: "link",
+      },
+    ],
+  },
+  community: {
+    title: `Community feel`,
+    text: `We are experts in creating a community`,
+    list: [
+      {
+        icon: `conv`,
+        text: `Q&A sessions are always highly rated at our events. People like to have conversations with speakers directly.`,
+      },
+      {
+        icon: `flag`,
+        text: `Our partners have an opportunity to reach out to the crowd - learn more about JetBrain's experience read`,
+      },
+      {
+        icon: `cap`,
+        text: `We offer hallway tracks, aka the Hidden Gem of Open Source, where everyone can meet fellow participants!`,
+      },
+      {
+        icon: `smile`,
+        text: `Of course, sparkles and bubbles are there - each of our conferences has a virtual party to share experiences`,
+      },
+    ],
+  },
+  youtube: {
+    title: `YouTube`,
+    text: `Hungry to learn more? Check out our Youtube channel.`,
+  },
+  leadership: {
+    title: `Our leadership`,
+    text: `Konfy is built by specialists and aficionados of software development, marketing & business smarties, people who believe in the solutions that matter. They commit their time, to save yours.`,
+    quote: `
+        <p>Oli has organized and participated in dozens of conference since 2013:</p>
+        <p>“My journey as an organizer started with a small scientific conference in Novosibirsk, Russia. Since then I’ve been focusing on building communities around the globe. 
+        How do we build caring community? In order to build a safe home, you need a strong construction. We are planning every detail of the conference to make it comfortable for everyone starting with Slack or Discord channels and ending with fun community parties!“<p>
+      `,
+  },
+  focus: {
+    title: `Our focus is community, learn what people say about us`,
+    text: `
+      <p>“#scalalove has many more people crossing the ocean than in-person conferences, and virtual conferences should remain part of the new normal.”</p>
+      <p>“Quite enjoying the #scalalove twitch stream. This conference talk format works well. The speakers look more relaxed, and the content has been very good so far.”</p>
+      <p>“Baking bread, cooking a tortilla de papas (Spanish omelette) and attending to the @scala_love conference. This is an awesome Saturday so far!”</p>
+      <p>“Scala Love is going smoother and technically better than many events we've been to. Great job @scala_love team!”</p>
+      <p>“Had a fantastic day participating in the Scala Love conference yesterday. No technical issues of any kind and great feeling of participating in real time.”</p>
+    `,
+    feedback: `More feedbacks`,
+  },
+  partnership: {
+    title: `Media partnership`,
+    text: `Konfy is built by specialists and aficionados of software development, marketing & business smarties, people who believe in the solutions that matter. They commit their time, to save yours.`,
+    button: `link`,
+  },
+  contacts: {
+    title: `Contacts`,
+    address: `5608 17th Avenue NW #921 Seattle, WA 98107`,
+    info: `info@konfy.care`,
+    partner: `partner@konfy.care`,
+    twitter: `twitter`,
+    patreon: `patreon`,
+    site: `site`,
+  },
+  privacy: `Privacy police`,
+  copyright: `© 2020 Konfy, LLC`,
+};
---
+++ src/pug.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  locals: {
+    i18n: require("./i18n/index.js"),
+  },
+};
